80457a598f4c1d6d0023ea637f61866ca2ac24f8
doctor-maxin <doctor-maxin@yandex.ru>
2023-09-08 06:07:33 +0700

    -

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: 'frontend',
             exec_mode: 'cluster',
             instances: 'max', // Or a number of instances
-            script: './node_modules/nuxt/bin/nuxt.js',
+            script: './node_modules/nuxi/bin/nuxi.mjs',
             args: 'start',
         }
     ]
